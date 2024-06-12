9546f45ab3d2d6c93cc0427f763d1aa60fd40b6a
Adao-Angelo <adaobegginer@gmail.com>
2024-06-12 18:39:32 +0100

    upadate readme file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./router.router";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

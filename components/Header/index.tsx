a52a0c7a0dfd9a83c25423b0b3101094fa6314c0
Adao-Angelo <adaobegginer@gmail.com>
2025-09-02 20:12:57 +0100

    update header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import { Folder, House, Info, User } from "lucide-react";
-import { useSearchParams } from "next/navigation";
 import MenuItem from "./MenuItem";
 
 export default function Header() {
-  const searchParams = useSearchParams();
-  const visibleSection = searchParams.get("visibleSection");
-
   return (
     <header className="p-6 fixed  justify-center  w-full flex">
       <nav className="flex justify-between p-2 bg-zinc-800/50 backdrop-blur-md rounded-full">
